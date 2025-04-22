31b6ff253fd9525e56b557e4c29ecdc9249772fc
Jonathan Allen <jon@karmasuitesoftware.com>
2025-04-22 15:35:40 -0700

    chore: upgrade some dependencies

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,10 +2,10 @@ const path = require("node:path");
 
 /** @type {import("drizzle-kit").Config} */
 const config = {
+    dialect: "sqlite",
     dbCredentials: {
         url: path.resolve(__dirname, "./sqlite.db"),
     },
-    driver: "better-sqlite",
     schema: "./src/schema/index.ts",
     strict: true,
     out: "./drizzle",
